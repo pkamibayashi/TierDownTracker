16d1afa8fd1a9d1885854d9150aa30caa25bd125
Paulo Kamibayashi <paulo_kamibayashi@hotmail.com>
2023-03-09 18:07:31 -0300

    changed connectDb

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import cors from "cors";
-import * as dotenv from "dotenv";
-import express from "express";
-import { connectToDB } from "./config/db.config.js";
-import machineRouter from "./routes/machine.routes.js";
-import partRouter from "./routes/part.routes.js";
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import express from 'express';
+import { connectToDB } from './config/db.config.js';
+import machineRouter from './routes/machine.routes.js';
+import partRouter from './routes/part.routes.js';
 
-import { userRouter } from "./routes/user.routes.js";
+import { userRouter } from './routes/user.routes.js';
 
 dotenv.config();
-connectToDB();
 
 const app = express();
 
@@ -17,8 +16,10 @@ app.use(express.json());
 
 app.use(`/user`, userRouter);
 app.use(`/part`, partRouter);
-app.use("/machine", machineRouter);
+app.use('/machine', machineRouter);
 
-app.listen(Number(process.env.PORT), () => {
-  console.log(`Server up and running at port ${process.env.PORT}`);
+connectToDB().then(() => {
+  app.listen(Number(process.env.PORT), () => {
+    console.log(`Server up and running at port ${process.env.PORT}`);
+  });
 });
